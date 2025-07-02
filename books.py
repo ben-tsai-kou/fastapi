47bcd54c2e2ae1835bfb4c09140755160a80d3ff
ben-tsai-kou <97225493+ben-tsai-kou@users.noreply.github.com>
2025-07-02 17:11:20 +0900

    post request

--- books.py
+++ books.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
 
 app = FastAPI()
 
@@ -43,3 +43,8 @@ async def read_author_category_by_author(book_author: str, category: str):
         ):
             books_to_return.append(book)
     return books_to_return
+
+
+@app.post("/books/create_book")
+async def create_book(new_book=Body()):
+    BOOKS.append(new_book)
