71905aba81bf1b0d64cab739f37fdaf1e1687bd1
ben-tsai-kou <97225493+ben-tsai-kou@users.noreply.github.com>
2025-07-02 17:00:03 +0900

    query params

--- books.py
+++ books.py
@@ -3,12 +3,12 @@ from fastapi import FastAPI
 app = FastAPI()
 
 BOOKS = [
-    {"title": "Title One", "author": "Author One", "category": "Category One"},
-    {"title": "Title Two", "author": "Author Two", "category": "Category Two"},
-    {"title": "Title Three", "author": "Author Three", "category": "Category Three"},
-    {"title": "Title Four", "author": "Author Four", "category": "Category Four"},
-    {"title": "Title Five", "author": "Author Five", "category": "Category Five"},
-    {"title": "Title Six", "author": "Author Six", "category": "Category Six"},
+    {"title": "Title One", "author": "AuthorOne", "category": "science"},
+    {"title": "Title Two", "author": "AuthorTwo", "category": "science"},
+    {"title": "Title Three", "author": "AuthorThree", "category": "history"},
+    {"title": "Title Four", "author": "AuthorFour", "category": "math"},
+    {"title": "Title Five", "author": "AuthorFive", "category": "math"},
+    {"title": "Title Six", "author": "AuthorSix", "category": "math"},
 ]
 
 
@@ -22,3 +22,24 @@ async def read_book(book_title: str):
     for book in BOOKS:
         if book.get("title").casefold() == book_title.casefold():
             return book
+
+
+@app.get("/books/")
+async def read_category_by_query(category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get("category").casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
+
+
+@app.get("/books/{book_author}/")
+async def read_author_category_by_author(book_author: str, category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if (
+            book.get("author").casefold() == book_author.casefold()
+            and book.get("category").casefold() == category.casefold()
+        ):
+            books_to_return.append(book)
+    return books_to_return
