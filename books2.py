3ab5547d4444d9b118e54c1d44d898c8a372d4b2
ben-tsai-kou <97225493+ben-tsai-kou@users.noreply.github.com>
2025-07-03 15:24:13 +0900

    Pydantic Configurations

--- books2.py
+++ books2.py
@@ -21,13 +21,29 @@ class Book:
 
 
 class BookRequest(BaseModel):
-    id: Optional[int] = None
+    id: Optional[int] = Field(description="ID is not needed on create", default=None)
     title: str = Field(min_length=3)
     author: str = Field(min_length=1)
     description: str = Field(
         min_length=1, max_length=100, description="The description of the book"
     )
     rating: int = Field(gt=0, lt=6)
+    test_title: Optional[str] = Field(
+        description="test field", alias="testTitle", default=""
+    )
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "title": "Harry Potter",
+                    "author": "J.K. Rowling",
+                    "description": "A book about a boy who goes to Hogwarts",
+                    "rating": 5,
+                }
+            ],
+        }
+    }
 
 
 BOOKS = [
