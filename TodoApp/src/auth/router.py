59b82cad808f55a33b4fd8766fc21fec27a391aa
ben-tsai-kou <97225493+ben-tsai-kou@users.noreply.github.com>
2025-07-06 18:18:22 +0900

    authenticate a user

--- TodoApp/src/auth/router.py
+++ TodoApp/src/auth/router.py
@@ -6,6 +6,7 @@ from src.auth import schema as auth_schema
 from src.auth import models as auth_models
 from starlette import status
 from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordRequestForm
 
 router = APIRouter()
 
@@ -24,6 +25,18 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
+def authenticate_user(username: str, password: str, db):
+    user = (
+        db.query(auth_models.User).filter(auth_models.User.username == username).first()
+    )
+
+    if not user:
+        return False
+    if not bcrypt_context.verify(password, user.hashed_password):
+        return False
+    return True
+
+
 @router.post("/auth", status_code=status.HTTP_201_CREATED)
 async def create_user(
     db: db_dependency, create_user_request: auth_schema.CreateUserRequest
@@ -40,3 +53,13 @@ async def create_user(
 
     db.add(create_user_model)
     db.commit()
+
+
+@router.post("/token")
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency
+):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        return "Failed Authentication"
+    return "Successful Authentication"
