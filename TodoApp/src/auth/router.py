0b382b9a1399387bd88091bb30e8aa5baccff00a
ben-tsai-kou <97225493+ben-tsai-kou@users.noreply.github.com>
2025-07-06 17:16:06 +0900

    Hash User’s Password

--- TodoApp/src/auth/router.py
+++ TodoApp/src/auth/router.py
@@ -1,8 +1,12 @@
 from fastapi import APIRouter
 from src.auth import schema as auth_schema
 from src.auth import models as auth_models
+from passlib.context import CryptContext
 
 router = APIRouter()
+
+
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 @router.post("/auth")
@@ -13,7 +17,7 @@ async def create_user(create_user_request: auth_schema.CreateUserRequest):
         first_name=create_user_request.first_name,
         last_name=create_user_request.last_name,
         role=create_user_request.role,
-        hashed_password=create_user_request.password,
+        hashed_password=bcrypt_context.hash(create_user_request.password),
         is_active=True,
     )
     return create_user_model
