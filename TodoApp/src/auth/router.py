4c87c65dba293016fae6c8b44252dbb96cff18a6
ben-tsai-kou <97225493+ben-tsai-kou@users.noreply.github.com>
2025-07-06 15:58:41 +0900

    create first user

--- TodoApp/src/auth/router.py
+++ TodoApp/src/auth/router.py
@@ -1,8 +1,19 @@
 from fastapi import APIRouter
+from src.auth import schema as auth_schema
+from src.auth import models as auth_models
 
 router = APIRouter()
 
 
-@router.get("/auth")
-async def get_user():
-    return {"user": "authenticated"}
+@router.post("/auth")
+async def create_user(create_user_request: auth_schema.CreateUserRequest):
+    create_user_model = auth_models.User(
+        email=create_user_request.email,
+        username=create_user_request.username,
+        first_name=create_user_request.first_name,
+        last_name=create_user_request.last_name,
+        role=create_user_request.role,
+        hashed_password=create_user_request.password,
+        is_active=True,
+    )
+    return create_user_model
