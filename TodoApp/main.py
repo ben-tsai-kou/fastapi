01dc6d0bb33a87819752a89e1f88550013d509d0
ben-tsai-kou <97225493+ben-tsai-kou@users.noreply.github.com>
2025-07-08 23:46:56 +0900

    admin router

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -5,6 +5,7 @@ from src.todo import models as todo_models
 from database import engine
 from src.auth import router as auth_router
 from src.todo import router as todo_router
+from src.admin import router as admin_router
 
 app = FastAPI()
 
@@ -13,3 +14,4 @@ todo_models.Base.metadata.create_all(bind=engine)
 
 app.include_router(auth_router.router)
 app.include_router(todo_router.router)
+app.include_router(admin_router.router)
