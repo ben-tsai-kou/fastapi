1c6e6323d58e4d0b7886ae7d53239be597639f53
ben-tsai-kou <97225493+ben-tsai-kou@users.noreply.github.com>
2025-07-18 07:51:17 +0900

    project test part 6

--- TodoApp/tests/admin/test_admin.py
+++ TodoApp/tests/admin/test_admin.py
@@ -2,6 +2,7 @@ from tests.utils import *
 from src.admin.router import get_db
 from src.auth.router import get_current_user
 from fastapi import status
+from src.todo import models as todo_models
 
 app.dependency_overrides[get_db] = override_get_db
 app.dependency_overrides[get_current_user] = override_get_current_user
@@ -20,3 +21,17 @@ def test_admin_read_all_authenticated(test_todo):
             "owner_id": 1,
         }
     ]
+
+
+def test_admin_delete_todo(test_todo, db):
+    response = client.delete("/admin/todo/1")
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+
+    model = db.query(todo_models.Todos).filter(todo_models.Todos.id == 1).first()
+    assert model is None
+
+
+def test_admin_delete_todo_not_found():
+    response = client.delete("/admin/todo/999")
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert response.json() == {"detail": "Todo not found."}
